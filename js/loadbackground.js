908b65da8514db2ad668a66e7837a1386b1928ef
EvaFarm <52913506+EvaFarm@users.noreply.github.com>
2020-08-12 18:04:38 +0300

    Add files via upload

--- js/loadbackground.js
+++ js/loadbackground.js
@@ -1,4 +1,4 @@
 "use strict"
 $(document).ready(function() {
-    $('body').css('background-image', 'url("../img/DSCF4573.JPG")');
+    document.getElementByClassName('bg').css('background-image', 'url("../img/DSCF4573.JPG")');
 });
